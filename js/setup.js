bdd5285256252c3c4022cfd1f114a344b85f43d9
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-09-28 08:37:45 +0300

    Fix namespaces

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var magesData = [];
-
 function arrayRandElement(arr) {
   var rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
@@ -12,42 +10,45 @@ var generateData = function (counter) {
   var secondNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+  var magesStat = [];
 
   for (var i = 1; i <= counter; i++) {
-    magesData.push({
+    magesStat.push({
       name: arrayRandElement(firstNames) + ' ' + arrayRandElement(secondNames),
       coatColor: arrayRandElement(coatColors),
       eyesColor: arrayRandElement(eyesColors),
     });
   }
+
+  return magesStat;
 };
 
-generateData(4);
+var magesData = generateData(4);
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+var similarMageTemplate = document.getElementById('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+var renderMage = function (mage) {
+  var mageElement = similarMageTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  mageElement.querySelector('.setup-similar-label').textContent = mage.name;
+  mageElement.querySelector('.wizard-coat').style.fill = mage.coatColor;
+  mageElement.querySelector('.wizard-eyes').style.fill = mage.eyesColor;
 
-  return wizardElement;
+  return mageElement;
 };
 
 var addSimiliarMages = function (magesArray) {
-  var fragment = document.createDocumentFragment();
+  var similarMagesFragment = document.createDocumentFragment();
   for (var i = 0; i < magesArray.length; i++) {
-    fragment.appendChild(renderWizard(magesArray[i]));
+    similarMagesFragment.appendChild(renderMage(magesArray[i]));
   }
-  similarListElement.appendChild(fragment);
+  similarListElement.appendChild(similarMagesFragment);
 };
 
 addSimiliarMages(magesData);
