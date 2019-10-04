2b6ea43b40718670f73407e0a8ac51ad3cf2feab
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-10-04 23:58:29 +0300

    Created Modules and Function to move the dialog box

---
+++ js/dragging.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function () {
+
+  window.dragging = {
+
+    mouseDraggingElement: function (evt, handler, element) {
+
+      evt.preventDefault();
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY,
+      };
+
+      var dragged = false;
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        dragged = true;
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY,
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY,
+        };
+
+        element.style.top = (element.offsetTop - shift.y) + 'px';
+        element.style.left = (element.offsetLeft - shift.x) + 'px';
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+
+        if (dragged) {
+          var onClickPreventDefault = function (clickEvt) {
+            clickEvt.preventDefault();
+            handler.removeEventListener('click', onClickPreventDefault);
+          };
+          handler.addEventListener('click', onClickPreventDefault);
+        }
+
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    },
+  };
+
+})();
