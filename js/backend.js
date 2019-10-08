0c2d55f41f91cebf875efb878806b42ff8294c26
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-10-08 09:07:12 +0300

    add XMLHttpRequest

---
+++ js/backend.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+
+  window.codeAndMagicApp.backend = {
+    load: function (onLoad, onError) {
+      var urlGet = 'https://js.dump.academy/code-and-magick/data';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status !== 200) {
+          onError('Статус ответа:' + xhr.status + ' ' + xhr.statusText);
+        } else {
+          onLoad(xhr.response);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за' + xhr.timeout + ' мс');
+      });
+
+      xhr.timeout = 5000;
+
+      xhr.open('GET', urlGet);
+      xhr.send();
+
+    },
+    save: function (data, onLoad, onError) {
+      var urlPost = 'https://js.dump.academy/code-and-magick';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status !== 200) {
+          onError('Статус ответа:' + xhr.status + ' ' + xhr.statusText);
+        } else {
+          onLoad();
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за' + xhr.timeout + ' мс');
+      });
+
+      xhr.timeout = 5000;
+
+      xhr.open('POST', urlPost);
+      xhr.send(data);
+    },
+  };
+
+
+})();
