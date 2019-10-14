18871e2a2bedc867fdabeb05e922052b5810a5ad
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-10-14 20:42:20 +0300

    Resolve bad naming and mutation

--- js/avatar.js
+++ js/avatar.js
@@ -4,14 +4,14 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var fileChooser = document.querySelector('.upload input[type=file]');
-  var preview = document.querySelector('.setup-user-pic');
 
   fileChooser.addEventListener('change', function () {
+    var preview = document.querySelector('.setup-user-pic');
     var file = fileChooser.files[0];
     var fileName = file.name.toLowerCase();
 
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
+    var matches = FILE_TYPES.some(function (fileType) {
+      return fileName.endsWith(fileType);
     });
 
     if (matches) {
